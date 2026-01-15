487d46c6b992ade95a132587474dde3f96d75be7
John Keller <john@nxt1sports.com>
2026-01-14 18:23:46 -0600

    refactor: remove unused infrastructure package

--- packages/infrastructure/src/network/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Network infrastructure barrel export
- * @module @nxt1/infrastructure/network
- */
-
-export { NetworkService } from './network.service';
-
-export type { ConnectionType, NetworkStatus, NetworkChangeEvent } from './network.service';
