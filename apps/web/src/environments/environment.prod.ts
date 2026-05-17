800459ebce4f5346be7de9d42671046e23485fc6
Nolan Nguyen <web.developer.gz@gmail.com>
2026-05-17 13:28:29 +0700

    fix(core): update version to 1.33.32 across all packages and environments

--- apps/web/src/environments/environment.prod.ts
+++ apps/web/src/environments/environment.prod.ts
@@ -5,8 +5,8 @@
  */
 export const environment = {
   production: true,
-  appVersion: '1.33.33',
-  version: '1.33.33',
+  appVersion: '1.33.32',
+  version: '1.33.32',
   webUrl: 'https://nxt1sports.com',
 
   // Backend API URLs (Production)
