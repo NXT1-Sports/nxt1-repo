38a3f90a2763d98b87f8cd2296286319be5df3b7
John Keller <john@nxt1sports.com>
2026-06-17 10:17:16 -0500

    revert: roll back app hosting lockfile attempts

--- apps/web/src/environments/environment.staging.ts
+++ apps/web/src/environments/environment.staging.ts
@@ -10,8 +10,8 @@
  */
 export const environment = {
   production: false,
-  appVersion: '1.62.17',
-  version: '1.62.17',
+  appVersion: '1.62.13',
+  version: '1.62.13',
   webUrl: 'https://nxt-1-staging-v2.web.app',
 
   // Backend API URLs (Staging)
