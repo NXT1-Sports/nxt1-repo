ba52870681220bd7cb8f8fb85e5ffd394f4416d1
Nolan Nguyen <web.developer.gz@gmail.com>
2026-07-03 10:32:19 +0700

    Revert main to v1.72.6

--- apps/web/src/environments/environment.prod.ts
+++ apps/web/src/environments/environment.prod.ts
@@ -5,8 +5,8 @@
  */
 export const environment = {
   production: true,
-  appVersion: '1.72.9',
-  version: '1.72.9',
+  appVersion: '1.72.6',
+  version: '1.72.6',
   webUrl: 'https://nxt1sports.com',
 
   // Backend API URLs (Production)
--- apps/web/src/environments/environment.ts
+++ apps/web/src/environments/environment.ts
@@ -6,8 +6,8 @@
  */
 export const environment = {
   production: false,
-  appVersion: '1.72.9-dev',
-  version: '1.72.9-dev',
+  appVersion: '1.72.6-dev',
+  version: '1.72.6-dev',
   webUrl: 'http://localhost:4200',
 
   // Backend API URLs (Local Development)
